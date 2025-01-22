77bece4ed15c6b8c7c501d2209cf44ac07a67a8f
Máta <fireroth42@gmail.com>
2025-01-22 10:02:57 +0100

    Main

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# CYD-Paint
+# Paint project for CYD (Cheap Yellow Display)
 
-https://github.com/rzeldent/platformio-espressif32-sunton/
+The included source code and bin file is made to run on ESP32-2432S028R
+
+For more information on this CYD and its variants, more example projects, mods, and more; check out [Brian Lough's ESP32-Cheap-Yellow-Display GitHub repo](https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display).
+
+### For porting to other boards
+
+[CYD (Cheap Yellow Display) PlatformIo Board definitions](https://github.com/rzeldent/platformio-espressif32-sunton/)
+
+---
+
+PLACEHOLDER IMAGE
---
+++ projects/paint/main.cpp
@@ -0,0 +1,89 @@
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+
+#define XPT2046_IRQ 36
+#define XPT2046_MOSI 32
+#define XPT2046_MISO 39
+#define XPT2046_CLK 25
+#define XPT2046_CS 33
+
+// You might have to change these values depending on your device
+#define TS_MINX 280
+#define TS_MAXX 3750
+#define TS_MINY 280
+#define TS_MAXY 3750
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+
+SPIClass mySpi(VSPI);
+TFT_eSPI tft = TFT_eSPI();
+XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
+
+uint16_t colors[] = {TFT_RED, TFT_GREEN, TFT_BLUE, TFT_YELLOW, TFT_CYAN, TFT_MAGENTA, TFT_WHITE};
+int currentColorIndex = 0;
+
+#define BUTTON_WIDTH 60
+#define BUTTON_HEIGHT 30
+#define BUTTON_X 0
+#define BUTTON_Y (SCREEN_HEIGHT - BUTTON_HEIGHT)
+
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize SPI for touchscreen
+  mySpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  ts.begin(mySpi);
+  ts.setRotation(1);
+
+  // Initialize TFT
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+
+  // Draw color selector boxes
+  for (int i = 0; i < 7; i++) {
+    tft.fillRect(i * (SCREEN_WIDTH / 7), 0, SCREEN_WIDTH / 7, 20, colors[i]);
+  }
+
+  // Draw the "Clear" button
+  tft.fillRect(BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT, TFT_WHITE);
+  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("CLEAR", BUTTON_X + BUTTON_WIDTH / 2, BUTTON_Y + BUTTON_HEIGHT / 2);
+}
+
+void loop() {
+  TS_Point p;
+
+  if (ts.tirqTouched() && ts.touched()) {
+    p = ts.getPoint();
+    if (p.z < 170) return;
+
+    // Map touch coordinates to screen dimensions
+    int x = map(p.x, TS_MINX, TS_MAXX, 0, SCREEN_WIDTH);
+    int y = map(p.y, TS_MINY, TS_MAXY, 0, SCREEN_HEIGHT);
+
+    if (y < 20) {
+      // Color selector area
+      currentColorIndex = x / (SCREEN_WIDTH / 7);
+    } else if (x >= BUTTON_X && x <= BUTTON_X + BUTTON_WIDTH && y >= BUTTON_Y && y <= BUTTON_Y + BUTTON_HEIGHT) {
+      // Clear screen button area
+      tft.fillScreen(TFT_BLACK);
+
+      // Redraw color selector boxes
+      for (int i = 0; i < 7; i++) {
+        tft.fillRect(i * (SCREEN_WIDTH / 7), 0, SCREEN_WIDTH / 7, 20, colors[i]);
+      }
+
+      // Redraw the "Clear Screen" button
+      tft.fillRect(BUTTON_X, BUTTON_Y, BUTTON_WIDTH, BUTTON_HEIGHT, TFT_WHITE);
+      tft.setTextColor(TFT_BLACK, TFT_WHITE);
+      tft.setTextDatum(MC_DATUM);
+      tft.drawString("CLEAR", BUTTON_X + BUTTON_WIDTH / 2, BUTTON_Y + BUTTON_HEIGHT / 2);
+    } else {
+      // Draw on the screen
+      tft.fillCircle(x, y, 3, colors[currentColorIndex]);
+    }
+  }
+}
